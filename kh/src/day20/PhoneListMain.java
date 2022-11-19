2d72662f3197ecea33e98af6864e18b7cc05dc6f
jeonghyeon <devhye@naver.com>
2022-11-19 18:43:43 +0900

    수정 완료

--- kh/src/day20/PhoneListMain.java
+++ kh/src/day20/PhoneListMain.java
@@ -3,7 +3,6 @@ package day20;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import java.util.function.Predicate;
 
 import lombok.Data;
 
@@ -23,6 +22,12 @@ class Phonebook{
 		this.Job = job;
 		count++;
 		num = count;
+	}
+	
+	public void update(String lastName, String firstName, String job) {
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.Job = job;
 	}
 	
 	public void insertPhone(String name, String Phonenumber) {
@@ -67,6 +72,11 @@ class Phone {
 	@Override
 	public String toString() {
 		return "장소:" + name + ", 번호:" + phoneNumber;
+	}
+
+	public void update(String name1, String phoneNumber1) {
+		this.name = name1;
+		this.phoneNumber = phoneNumber1;
 	}
 	
 	
@@ -126,10 +136,12 @@ public class PhoneListMain {
 			}
 			break;
 		case 2:
-			System.out.println("2. 전화번호 수정");
+			Phonebook tmp =select(phonebookList, sc);
+			printsubmenu();
+			int submenuNum = sc.nextInt();
+			subMenu(tmp, sc, submenuNum, phonebookList, phoneList);
 			break;
 		case 3:
-			System.out.println("3. 전화번호 삭제");
 			break;
 		case 4:
 			System.out.println("4. 전화번호 조회");
@@ -140,7 +152,46 @@ public class PhoneListMain {
 			System.out.println("잘못된 메뉴입니다.");
 		}
 	}
-	public static void select(ArrayList<Phonebook> phonebookList,Scanner sc) {
+	public static void printsubmenu() {
+		System.out.println("1. 이름,직장 수정");
+		System.out.println("2. 기존 전화번호 수정");
+		System.out.println("3. 새 전화번호 추가");
+		System.out.print("메뉴선택:");
+	}
+
+	private static void subMenu(Phonebook tmp, Scanner sc,int submenuNum,ArrayList<Phonebook> pb,ArrayList<Phone> phone) {
+		switch (submenuNum) {
+		case 1://이름 직장
+			Phonebook p = inputPhonebook(sc);
+			tmp.update(p.getLastName(), p.getFirstName(), p.getJob());
+			System.out.println("이름 직장을 수정 하였습니다.");
+			break;
+		case 2://기존 전화번호
+				String name1,phoneNumber1;
+				for (int i = 0; i < tmp.getPhones().size(); i++) {
+					System.out.println((i+1)+""+tmp.getPhones().get(i));
+				}
+				System.out.print("수정할 전화번호를 선택해주세요>>");
+				int num =sc.nextInt();
+				Phone ptmp = tmp.getPhones().get(num-1);
+				System.out.println("수정을 시작합니다.");
+				System.out.print("이름: "); name1 = sc.next();
+				System.out.print("번호: "); phoneNumber1 = sc.next();
+				tmp.getPhones().get(num).update(name1,phoneNumber1);
+				System.out.println("기존 전화번호를 수정하였습니다.");
+				break;
+		case 3://새 전화번호 추가
+			String name,phoneNumber;
+			System.out.print("이름: "); name = sc.next();
+			System.out.print("번호: "); phoneNumber = sc.next();
+			tmp.insertPhone(name, phoneNumber);
+			pb.add(tmp);
+			System.out.println("새 번호를 추가하였습니다.");
+			break;
+		}
+	}
+	
+	public static Phonebook select(ArrayList<Phonebook> phonebookList,Scanner sc) {
 		String lastName,firstName;
 		System.out.print("성: "); lastName = sc.next();
 		System.out.print("이름: "); firstName = sc.next();
@@ -154,7 +205,8 @@ public class PhoneListMain {
 		System.out.print("자세히 보고싶은 전화번호부 번호를 입력하세요>>");
 		int num = sc.nextInt();
 		System.out.println(phonebookList.get(num-1));
-	
+		Phonebook ppp = phonebookList.get(num-1);
+		return ppp;
 	}
 
 	public static Phonebook inputPhonebook(Scanner sc) {
@@ -164,22 +216,21 @@ public class PhoneListMain {
 		System.out.print("이름: "); firstName = sc.next();
 		System.out.print("직장: "); Job = sc.next();
 		return new Phonebook(lastName, firstName, Job);
-		
 	}
 
 	public static void inputPhones(Scanner sc, Phonebook inputPhonebook,ArrayList<Phonebook> phonebookList) {
 		inputPhonebook = inputPhonebook(sc);
 		char insert;
 		do {
-		String name;
-		String phoneNumber;
-		System.out.print("이름: "); name = sc.next();
-		System.out.print("번호: "); phoneNumber = sc.next();
-		System.out.println("전화번호를 더 등록 하시겠습니까?");
-		insert = sc.next().charAt(0);
-		inputPhonebook.insertPhone(name, phoneNumber);
+			String name;
+			String phoneNumber;
+			System.out.print("이름: "); name = sc.next();
+			System.out.print("번호: "); phoneNumber = sc.next();
+			System.out.println("전화번호를 더 등록 하시겠습니까?");
+			insert = sc.next().charAt(0);
+			inputPhonebook.insertPhone(name, phoneNumber);
 		}while(insert != 'n');
-		phonebookList.add(inputPhonebook);
+			phonebookList.add(inputPhonebook);
 	}
 
 	public static void printMenu() {
@@ -190,12 +241,4 @@ public class PhoneListMain {
 		System.out.println("4. 전화번호 조회");
 		System.out.print("메뉴선택:");
 	}
-	
-//	public static void detailSelect(ArrayList<Phonebook> phoneList,Predicate<Phonebook> p) {
-//		for (Phonebook tmp : phoneList) {
-//			if(p.test(tmp)) {
-//				System.out.println("["+(tmp.getNum()+1)+"]"+tmp.getLastName()+tmp.getFirstName());
-//			}
-//		}
-//	}
 }
