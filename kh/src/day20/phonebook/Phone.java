0deac186ab2881d59674213f710cd6035df71250
jeonghyeon <devhye@naver.com>
2022-11-20 21:59:03 +0900

    주말 전화번호부 프로그램 과제 다른방법으로 해보기 수정전까지 함

---
+++ kh/src/day20/phonebook/Phone.java
@@ -0,0 +1,18 @@
+package day20.phonebook;
+
+import lombok.Data;
+
+@Data
+public class Phone {
+	
+	private String name;
+	private String number;
+	
+	public Phone(String name, String number) {
+		this.name = name;
+		this.number = number;
+	}
+	
+	
+
+}
