a32dc7c8d9da0350f01ea1c9ea9c6301000be21e
jeonghyeon <devhye@naver.com>
2022-11-27 01:05:35 +0900

    메소드 7-10

---
+++ java200/chap7_method/MentalArithmetic.java
@@ -0,0 +1,14 @@
+package chap7_method;
+
+import java.util.Scanner;
+
+class MentalArithmetic {
+	static Scanner sc = new Scanner(System.in);
+
+	// 계속할지 여부 확인
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
