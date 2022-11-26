8bb5c5a66c255faf0e4a2c861401fa412b088799
jeonghyeon <devhye@naver.com>
2022-11-27 01:05:16 +0900

    주말과제 게시판 관리 프로그램 하는중

---
+++ kh/src/example/board/Board.java
@@ -0,0 +1,29 @@
+package example.board;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Board implements Serializable{
+	private static int boardNum = 0; // 게시글 번호
+	private int num;
+	private String title; // 제목
+	private String contents; // 내용
+	private int views; // 조회수
+	private int like; // 좋아요
+	private Date date;
+	private Member member; // 게시판은 멤버 정보를 가지고있다.
+	
+	public Board(String title, String contents, Member member) {
+		this.title = title;
+		this.contents = contents;
+		date = new Date();
+		++boardNum;
+		num = boardNum;
+		this.member = member;
+	}
+	
+}
---
+++ kh/src/example/board/BoardManagementProgram.java
@@ -0,0 +1,246 @@
+package example.board;
+
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class BoardManagementProgram {
+	/*주말 과제3 게시물 관리 프로그램
+	 * - 게시글 종류는 자유 , 공지는 기본
+	 * - 게시글 종류는 추가 가능
+	 * - 게시글은 회원만 작성 가능
+	 * - 회원가입, 로그인 기능 필요 ----> 완료
+	 * - 비회원은 작성, 수정 불가능 , 조회 가능
+	 * - 회원 정보와 게시글을 파일에 저장하여 관리 ----> 회원 정보 완료/ 
+	 * */
+	private static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		String fileName = "member.txt";
+		int menu = -1;
+		ArrayList<Member> memberList = new ArrayList<>();
+		load(memberList,fileName);
+		do {
+			printMainMenu();
+			try {
+				menu = sc.nextInt();
+			}catch (InputMismatchException e) {
+				sc.nextLine(); // 잘못 입력한거 날려버리기
+				System.out.println("정수로 다시 입력 해주세요!!");
+				System.out.print("메뉴 선택>>");
+				menu = sc.nextInt();
+			}
+			runMainMenu(memberList,menu);
+		}while(menu != 3);
+		save(memberList,fileName);
+	}
+
+
+
+	private static void runMainMenu(ArrayList<Member> memberList, int menu) {
+		switch (menu) {
+		case 1://회원가입
+			join(memberList);
+			break;
+		case 2://로그인
+			Member loginMember = login(memberList);
+			if(loginMember != null)
+				board(loginMember); // 로그인에 성공하면 게시판 메소드로 이동
+			break;
+		default:
+		}
+	}
+								//로그인 한 객체
+	private static void board(Member loginMember) {
+		ArrayList<Board> boardList = new ArrayList<>();
+		String fileName = "board.txt";
+		load(boardList,fileName);
+		int menu = -1;
+		boardsubMenu();
+		do {
+			try {
+				menu = sc.nextInt();
+				runboardSubMenu(menu,loginMember,boardList);
+			}catch (InputMismatchException e) {
+				sc.nextLine(); // 잘못 입력한거 날려버리기
+				System.out.println("정수로 다시 입력 해주세요!!");
+				System.out.print("메뉴 선택>>");
+				menu = sc.nextInt();
+			}
+		}while(menu !=0);
+		save(boardList,fileName);
+	
+	}
+
+
+
+	private static void runboardSubMenu(int menu, Member loginMember, ArrayList<Board> boardList) {
+		switch (menu) {
+		case 1: //게시글 작성
+			insert(loginMember);
+			break;
+		case 2://게시물 목록
+			boardAll(boardList);
+			break;
+		case 0:
+			break;
+		default:
+		}
+	}
+
+
+
+	private static void boardAll(ArrayList<Board> boardList) {
+		if(boardList == null) 
+			throw new RuntimeException("예외 발생 : 게시판을 관리할 리스트가 생성 되지 않았습니다.");
+		for (Board board : boardList) {
+			System.out.println(board);
+		}
+	}
+
+
+
+	private static void boardsubMenu() {
+		System.out.println("===============");
+		System.out.println("1. 게시글 작성");
+		System.out.println("2. 게시글 목록");
+		System.out.println("===============");
+		System.out.print("메뉴 선택>>");
+	}
+
+
+
+	private static void insert(Member loginMember) {
+		ArrayList<Board> boardList = new ArrayList<>();
+		if(loginMember == null) {
+			System.out.println("로그인 정보가 없습니다.");
+		}
+		System.out.println("게시글을 작성 합니다.");
+		sc.nextLine();
+		System.out.print("제목:");String title = sc.nextLine();
+		System.out.print("내용:");String contents = sc.nextLine();
+		Board insertBoard = new Board(title, contents, loginMember);
+		loginMember.addBoard(insertBoard);
+		System.out.println(loginMember.toString());
+		
+	}
+
+
+
+	private static Member login(ArrayList<Member> memberList) {
+		if(memberList == null) 
+			throw new RuntimeException("예외 발생 : 멤버를 관리할 리스트가 생성 되지 않았습니다.");
+		System.out.println("로그인 해주세요.");
+		System.out.print("아이디:"); String id = sc.next();
+		System.out.print("비밀번호:"); String pw = sc.next();
+		
+		Member loginMember = search(memberList, (m) -> m.getId().equals(id) && m.getPassword().equals(pw));
+		
+		// 입력한 아이디 비밀번호가 일치하는지?
+		for (int i = 0; i < memberList.size(); i++) {
+			if(memberList.get(i).getId().equals(id) && memberList.get(i).getPassword().equals(pw)) {
+				System.out.println("로그인에 성공!!");
+			}else {
+				System.out.println("로그인에 실패!!");
+			}
+			
+		}
+		return loginMember; // 로그인에 성공하면 로그인한 아이디 비밀번호 정보를 가져간다.
+		
+	}
+
+
+	private static <T> T search(ArrayList<T> list,Predicate<T> p) {
+		for (int i = 0; i < list.size(); i++) {
+			if(p.test(list.get(i))) {
+				return list.get(i);
+			}
+		}
+		return null;
+	}
+
+
+
+	private static <T> void save(ArrayList<T> list, String fileName) {
+		if(list == null) 
+			throw new RuntimeException("예외 발생 : 리스트가 생성 되지 않았습니다.");
+	
+		try(ObjectOutputStream oos
+			= new ObjectOutputStream(new FileOutputStream(fileName))) {
+			for (T t : list) {
+				oos.writeObject(t);
+			}
+			System.out.println("저장하기 완료");
+		} catch (FileNotFoundException e) {
+			System.out.println(fileName + "을 생성할 수 없어서 저장에 실패했습니다.");
+		} catch (IOException e) {
+			System.out.println("저장에 실패했습니다.");
+		}
+	
+	}
+	
+	private static <T> void load(ArrayList<T> list, String fileName) {
+		if(list == null) 
+			throw new RuntimeException("예외 발생 : 리스트가 생성 되지 않았습니다.");
+	
+		try(ObjectInputStream ois
+			= new ObjectInputStream(new FileInputStream(fileName))) {
+			while(true) {
+				T t = (T) ois.readObject();
+				list.add(t);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println(fileName + "이 없어서 불러오기에 실패했습니다.");
+		} catch (EOFException e) {
+			System.out.println("불러오기 완료.");
+		} catch (Exception e) {
+			System.out.println("불러오기 실패.");
+		}
+	}
+	
+	
+	private static void printMainMenu() {
+		System.out.println("===============");
+		System.out.println("1. 회원가입");
+		System.out.println("2. 로그인");
+		System.out.println("3. 프로그램 종료");
+		System.out.println("===============");
+		System.out.print("메뉴 선택>>");
+	}
+	
+	private static void join(ArrayList<Member> memberList) {
+		if(memberList == null) 
+			throw new RuntimeException("예외 발생 : 멤버를 관리할 리스트가 생성 되지 않았습니다.");
+		
+		System.out.println("회원가입을 시작 합니다.");
+		System.out.print("아이디:");String id = sc.next();
+		System.out.print("비밀번호:");String pw = sc.next();
+		System.out.print("닉네임:");String nickname = sc.next();
+		Member member = new Member(id, pw, nickname);
+		if(memberList.indexOf(member) != -1) { 
+			System.out.println("회원 정보를 추가하지 못했습니다.");
+			return ;
+		}
+		memberList.add(member);
+		System.out.println("회원 정보를 추가했습니다.");
+		
+		//확인용
+		for (Member m : memberList) {
+			System.out.println(m.print());
+		}
+	}
+
+
+
+
+
+
+}
