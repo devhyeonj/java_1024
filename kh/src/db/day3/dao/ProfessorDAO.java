17aec79f2f2c06b31ae4bb6b85ad6b39b40013d9
Jeonghyeon Lee <devhye@naver.com>
2023-01-16 12:03:51 +0900

    Delete kh/src/db directory

--- kh/src/db/day3/dao/ProfessorDAO.java
+++
@@ -1,9 +0,0 @@
-package db.day3.dao;
-
-import db.day3.vo.ProfessorVO;
-
-public interface ProfessorDAO {
-	
-	void insertProfessor(ProfessorVO professorVO);
-	void updateProfessor(ProfessorVO professorVO);
-}
--- kh/src/db/day3/service/ProfessorService.java
+++
@@ -1,8 +0,0 @@
-package db.day3.service;
-
-public interface ProfessorService {
-	void insertProfessor();
-	void updateProfessor();
-	void updateAdvisor();
-
-}
