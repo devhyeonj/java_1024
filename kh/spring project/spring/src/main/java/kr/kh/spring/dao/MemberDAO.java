8917590f9c0de82a29e76fdcd427c0a4d942efd6
aaaa <devhye@naver.com>
2023-02-13 20:58:34 +0900

    스프링 4일차

--- kh/spring project/spring/src/main/java/kr/kh/spring/dao/MemberDAO.java
+++ kh/spring project/spring/src/main/java/kr/kh/spring/dao/MemberDAO.java
@@ -2,8 +2,16 @@ package kr.kh.spring.dao;
 
 import org.apache.ibatis.annotations.Param;
 
+import kr.kh.spring.vo.MemberOKVO;
+import kr.kh.spring.vo.MemberVO;
+
 public interface MemberDAO {
 
 	String selectMemberName(@Param("num")String num);
+
+	int insertMember(@Param("m") MemberVO member); 	
+	// param으로 m으로 했기때문에 mapper.xml에서 m.으로 접근한거임
+
+	void insertMember(@Param("mok") MemberOKVO mok);
 	
 }
