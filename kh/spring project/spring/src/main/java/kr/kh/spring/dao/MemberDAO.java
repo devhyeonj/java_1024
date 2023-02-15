ad5bfe484420e34b1ecd9af44922d1859e1aba89
aaaa <devhye@naver.com>
2023-02-15 16:06:04 +0900

    스프링 5일차

--- kh/spring project/spring/src/main/java/kr/kh/spring/dao/MemberDAO.java
+++ kh/spring project/spring/src/main/java/kr/kh/spring/dao/MemberDAO.java
@@ -7,11 +7,16 @@ import kr.kh.spring.vo.MemberVO;
 
 public interface MemberDAO {
 
-	String selectMemberName(@Param("num")String num);
+	int insertMember(@Param("m")MemberVO member);
 
-	int insertMember(@Param("m") MemberVO member); 	
-	// param으로 m으로 했기때문에 mapper.xml에서 m.으로 접근한거임
+	void insertMemberOK(@Param("mok")MemberOKVO mok);
 
-	void insertMember(@Param("mok") MemberOKVO mok);
-	
+	MemberOKVO selectMemberOK(@Param("mok")MemberOKVO mok);
+
+	void deleteMemberOK(@Param("mok")MemberOKVO mok);
+
+	void updateAuthority(@Param("me_id")String me_id, @Param("me_authority")int me_authority);
+
+	MemberVO selectMemberById(@Param("me_id") String me_id);
+
 }
--- kh/spring project/spring/src/main/java/kr/kh/spring/service/MemberServiceImp.java
+++ kh/spring project/spring/src/main/java/kr/kh/spring/service/MemberServiceImp.java
@@ -1,6 +1,12 @@
 package kr.kh.spring.service;
 
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import kr.kh.spring.dao.MemberDAO;
@@ -8,41 +14,109 @@ import kr.kh.spring.vo.MemberOKVO;
 import kr.kh.spring.vo.MemberVO;
 
 @Service
-public class MemberServiceImp implements MemberService{
+public class MemberServiceImp implements MemberService {
 	@Autowired
-	MemberDAO memberDAO;
+	MemberDAO memberDao;
+	@Autowired
+	private JavaMailSender mailSender;
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
 
 	@Override
 	public boolean signup(MemberVO member) {
-		if(member == null) 
+		if(member == null)
 			return false;
-		if(memberDAO.insertMember(member) != 0)
+		//비밀번호 암호화
+		String newPw = passwordEncoder.encode(member.getMe_pw()); // 인코더가 암호화 해줌
+		member.setMe_pw(newPw);
+		
+		if(memberDao.insertMember(member) != 0)
 			return true;
 		return false;
 	}
 
 	@Override
-	public void emailAuthentication(String me_id) {
+	public void emailAuthentication(String me_id, String me_email) {
+		String str = authenticationNumber();
+		MemberOKVO mok = new MemberOKVO(me_id, str);
+		memberDao.insertMemberOK(mok);
+		
+		String title = "[Spring]이메일 인증 메일입니다.";
+		String content = 
+			"다음 링크를 클릭해서 이메일 인증을 완료하세요.<br>" + 
+			"<a href='http://localhost:8080/spring/email?mo_num="+str+"&mo_me_id="+me_id+"'>이메일 인증하기</a>";
+		//이메일 전송
+		sendEmail(title, content, me_email);
+	}
+	private String authenticationNumber() {
 		//인증번호 생성 : 6자리, 영어, 숫자로 된 인증 번호
 		String str = "";
 		int max = 61, min = 0;
-		// 영어 소문자 : 26개 , 영어 대문자 : 26개 , 숫자 : 10개 총 62개
+		//영어 소문자 : 26개, 영어 대문자 : 26개, 숫자 : 10개 총 : 62개
 		while(str.length() != 6) {
-			int r = (int)(Math.random() * (max-min+1)+min);
+			int r = (int)(Math.random()*(max-min + 1) + min);
 			//숫자 0~9
 			if(r <= 9) {
 				str += r;
 			}
-			//영어 소문자 10~35. 10=>a , 11=>b,.... 35=>z
-			else if(r <= 35){
-				str += (char) 'a'+(r-10);
+			//영어 소문자 10~35. 10=>a, 11=>b,... 35=>z
+			else if(r <= 35) {
+				str += (char)('a'+(r-10));
 			}
 			else {
-				str += (char) 'a'+(r-35);
+				str += (char)('A'+(r-36));
 			}
 		}
-		MemberOKVO mok = new MemberOKVO(me_id,str);
-		memberDAO.insertMember(mok);
+		return str;
+	}
+	private void sendEmail(String title, String content, String email) {
+		String setfrom = "stajun@gmail.com";         
+	    
+	    try {
+	        MimeMessage message = mailSender.createMimeMessage();
+	        MimeMessageHelper messageHelper 
+	            = new MimeMessageHelper(message, true, "UTF-8");
+
+	        messageHelper.setFrom(setfrom);  // 보내는사람 생략하거나 하면 정상작동을 안함
+	        messageHelper.setTo(email);     // 받는사람 이메일
+	        messageHelper.setSubject(title); // 메일제목은 생략이 가능하다
+	        //setText에 true를 추가하면, content에 있는 html코드를 html코드로 적용
+	        messageHelper.setText(content, true);  // 메일 내용
+
+	        mailSender.send(message);
+	    } catch(Exception e){
+	        System.out.println(e);
+	    }
 	}
 
+	@Override
+	public boolean emailAuthenticationConfirm(MemberOKVO mok) {
+		if(mok == null)
+			return false;
+		MemberOKVO dbMok = memberDao.selectMemberOK(mok);
+		if(dbMok != null) {
+			//member_ok 테이블에서 해당 데이터를 삭제하고
+			memberDao.deleteMemberOK(mok);
+			//member테이블에서 해당 회원의 권한을 1로 등급업
+			memberDao.updateAuthority(mok.getMo_me_id(), 1);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public MemberVO login(MemberVO member) {
+		if(member == null || member.getMe_id() == null
+				|| member.getMe_pw() == null)
+				return null;
+		//비밀번호는 암호화 되어있어서 selectMember로 못가져옴
+		MemberVO dbMember = memberDao.selectMemberById(member.getMe_id());
+		
+		if(dbMember == null) 
+			return null;
+		// 변환이 가능한지 true false로 알려줌 복호화해서 비교할 필요가 없음
+		if(passwordEncoder.matches(member.getMe_pw(), dbMember.getMe_pw())) 
+			return dbMember;
+		return null;
+	}
 }
