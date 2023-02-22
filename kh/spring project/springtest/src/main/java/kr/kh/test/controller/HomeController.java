a5cc4eb5e5ee5f18261918e9a4af19ee163e9a38
aaaa <devhye@naver.com>
2023-02-22 17:05:24 +0900

    스프링 복습 10일차 : 메시지 클래스 추가 및 적용

--- kh/spring project/springtest/src/main/java/kr/kh/test/controller/HomeController.java
+++ kh/spring project/springtest/src/main/java/kr/kh/test/controller/HomeController.java
@@ -1,6 +1,7 @@
 package kr.kh.test.controller;
 
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import kr.kh.test.controller.utils.MessageUtils;
 import kr.kh.test.service.MemberService;
 import kr.kh.test.vo.MemberOKVO;
 import kr.kh.test.vo.MemberVO;
@@ -22,6 +24,8 @@ public class HomeController {
 	
 	@Autowired
 	MemberService memberService;
+	
+	String contextPath = "/test";
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView home(ModelAndView mv, Integer num) {
@@ -37,16 +41,16 @@ public class HomeController {
 	}
 	
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public ModelAndView signupPost(ModelAndView mv,MemberVO member) {
+	public ModelAndView signupPost(ModelAndView mv,MemberVO member,HttpServletResponse response) {
 		System.out.println(member); // 확인
 	
 		boolean isSignup = memberService.signup(member);
 		if(isSignup) {
-			//성공했다고 알림 메세지(추후 구현 예정)
+			 MessageUtils.alertAndMovePage(response, "회원가입에 성공했습니다.", contextPath, "/");
 			mv.setViewName("redirect:/");
 		}else {
-			//실패했다고 알림 메세지(추후 구현 예정)
-			mv.setViewName("/member/signup");
+			 MessageUtils.alertAndMovePage(response, "회원가입에 실패했습니다.", contextPath, "/signup");
+			mv.setViewName("redirect:/signup");
 		}
 		return mv;
 	}
@@ -58,37 +62,46 @@ public class HomeController {
 	}
 	
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public ModelAndView login(ModelAndView mv,MemberVO member) {
+	public ModelAndView login(ModelAndView mv,MemberVO member,HttpServletResponse response) {
 		 MemberVO user = memberService.login(member);
 		 // 그외의 다른정보들을 세션에 저장해놓았다가 로그인 할때마다 꺼내쓰기 위해서 객체를 씀
 		 //인증한 회원들만 로그인 하도록 
 		 if(user != null && user.getMe_authority() > 0) {
 			 mv.addObject("user", user);
 			 mv.setViewName("redirect:/");
+			 MessageUtils.alertAndMovePage(response, "로그인에 성공했습니다.", contextPath, "/");
 		 }
 		 else {
+			 if(user != null) {
+				 //인증 안된 회원이라고 알려주는 메세지
+				 MessageUtils.alertAndMovePage(response, "이메일 인증을 완료해야 로그인이 로그인이 가능합니다.", contextPath, "/login");
+			 }else {
+				 MessageUtils.alertAndMovePage(response, "로그인에 실패했습니다.", contextPath, "/login");
+			 }
 			 mv.setViewName("redirect:/login"); //url입력하는거임 /member/login x
 		 }
 		return mv;
 	}
 	@RequestMapping(value = "/logout")
-	public ModelAndView logout(ModelAndView mv,HttpSession session) {
+	public ModelAndView logout(ModelAndView mv,HttpSession session,HttpServletResponse response) {
 		MemberVO member	= (MemberVO) session.getAttribute("user");
 		if(member != null) {
 			session.removeAttribute("user");
-			mv.setViewName("redirect:/");
-			
+			 MessageUtils.alertAndMovePage(response, "로그아웃에 성공했습니다.", contextPath, "/");
 		}
+		mv.setViewName("redirect:/");
 		return mv;
 	}
 	
 	@RequestMapping(value = "/email/authentication", method = RequestMethod.GET) 
-	public ModelAndView emailAuthentication(ModelAndView mv,MemberOKVO mok) {
+	public ModelAndView emailAuthentication(ModelAndView mv,MemberOKVO mok,HttpServletResponse response) {
 		boolean res = memberService.emailAuthentication(mok);
 		if(res) {
 			//인증 성공 메세지
+			 MessageUtils.alertAndMovePage(response, "이메일 인증에 성공했습니다.", contextPath, "/");
 		}else {
 			//인증 실패 메세지
+			 MessageUtils.alertAndMovePage(response, "이메일 인증에 실패했습니다.", contextPath, "/");
 		}
 		mv.setViewName("redirect:/");
 		return mv;
